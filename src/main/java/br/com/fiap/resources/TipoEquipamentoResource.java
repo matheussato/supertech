97e8fef4e8fed8b9c5d60e9efac805f3390d5794
Matheus Sato <dssato03@gmail.com>
2023-03-15 10:21:57 -0300

    feat: "ADD: TipoEquipamentoResource.java"

---
+++ src/main/java/br/com/fiap/resources/TipoEquipamentoResource.java
@@ -0,0 +1,19 @@
+package br.com.fiap.resources;
+
+import br.com.fiap.domain.equipamento.repository.TipoEquipamentoRepository;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/tipo-equipamento")
+public class TipoEquipamentoResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll(){
+        return Response.ok(TipoEquipamentoRepository.findAll()).build();
+    }
+
+}
